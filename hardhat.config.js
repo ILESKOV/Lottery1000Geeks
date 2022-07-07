7d94f5c655dce643e5a2907321819e918deb145e
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-07-07 17:54:02 +0200

    Config
    Networks added: Ethereum and Rinkeby testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("@nomiclabs/hardhat-solhint");
 require("@nomiclabs/hardhat-etherscan");
 const url = require("./secret").url;
 const key = require("./secret").key;
+const key = require("./secret").api;
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -13,23 +14,23 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 });
 
 module.exports = {
-  solidity: "0.8.13",
+  solidity: "0.8.15",
   defaultNetwork: "hardhat",
   networks: {
-   hardhat: {
-     initialBaseFeePerGas: 0,
-      },
-   rinkeby: {
-     url: `https://rinkeby.infura.io/v3/${url}`,
-     accounts: [`0x${key}`]
-   },
-   ethereum: {
-     chainId: 1,
-     url: `https://mainnet.infura.io/v3/${url}`,
-     accounts: [`0x${key}`]
-   },
+    hardhat: {
+      initialBaseFeePerGas: 0,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${url}`,
+      accounts: [`0x${key}`],
+    },
+    ethereum: {
+      chainId: 1,
+      url: `https://mainnet.infura.io/v3/${url}`,
+      accounts: [`0x${key}`],
+    },
   },
-//    etherscan: { 
-//    apiKey: process.env.ETHERSCAN_API_KEY,
-//  },
+  etherscan: {
+    apiKey: api,
+  },
 };
