5b2aefbaf01c4c200c39b30c9ac70c86f99e5a5b
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-07-07 17:49:48 +0200

    deploying script
    Addresses are from RInkeby Testnet

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,24 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  keyhash =
+    "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc";
+  aggregator = "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e";
+  vrfCoordinator = "0x6168499c0cFfCaCD319c818142124B7A15E857ab";
 
-  // We get the contract to deploy
   const Lottery = await hre.ethers.getContractFactory("Lottery");
-  const lottery = await Lottery.deploy("Hello, Hardhat!");
+  const lottery = await Lottery.deploy(
+    8029,
+    aggregator,
+    vrfCoordinator,
+    keyhash
+  );
 
   await lottery.deployed();
 
   console.log("Lottery deployed to:", lottery.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
